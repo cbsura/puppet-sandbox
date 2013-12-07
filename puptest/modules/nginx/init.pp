48438874d0bc673a8170eb2b7b305754557b2a19
cbsura <csura@snet.net>
2013-12-07 16:58:14 -0500

    First git push

---
+++ puptest/modules/nginx/init.pp
@@ -0,0 +1,16 @@
+# puppet msnifest
+class nginx {
+  package { 'apache2.2-common':
+    ensure => absent,
+  }
+
+  package { 'nginx':
+    ensure => installed,
+    require => Package['apache2.2-common'],
+  }
+
+  service { 'nginx':
+    ensure  => running,
+    require => Package['nginx'],
+  }
+}
