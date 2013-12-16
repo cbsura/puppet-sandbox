c59384e94cc8dc978407a0cc84b8298ba001fc6a
cbsura <csura@snet.net>
2013-12-15 22:25:57 -0500

    Added comments

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,13 @@
 # /etc/puppet/manifests/site.pp
 # This is the first manifest that the master will parse
+# We distinguish the master node from its agents
 
 # This is the special case for our puppet master node
 node 'sura-puppet.sura-puppet.b3.internal.cloudapp.net' {
-  file { '/tmp/hello':
-    content => "Hello, I am puppet master ${::fqdn}\n",
-  }
+  file { '/tmp/hello': content => "Hello, I am puppet master ${::fqdn}\n", }
 }
 
 # This is the default case for our puppet agent nodes
 node default {
-  file { '/tmp/hello':
-    content => "Hello, I am puppet agent ${::fqdn}\n",
-  }
+  file { '/tmp/hello': content => "Hello, I am puppet agent ${::fqdn}\n", }
 }
